8dbf280241c45ca3e3def0791a5548343e1641f5
Enk-sho portfolio <emshowah3@gmail.com>
2026-07-14 21:18:21 -0300

    Add files via upload

---
+++ automated web scraper.py
@@ -0,0 +1,17 @@
+import requests
+from bs4 import BeautifulSoup
+
+# Inspect the destination page
+
+# Extraction script
+# Import core modules 
+
+# Download Page content
+
+# Parse Raw HTML 
+
+# Export Data to a structured File
+
+# Open Document File
+
+# Automate schedule
