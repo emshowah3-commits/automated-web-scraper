1c179729bea49b2d6449b689981cd8c86cbac0c7
Enk-sho portfolio <emshowah3@gmail.com>
2026-07-19 20:39:34 -0300

    Add files via upload

--- automated web scraper.py
+++ automated web scraper.py
@@ -1,17 +1,30 @@
 # Import core modules 
 import requests
 from bs4 import BeautifulSoup
-
+import csv
 # Extraction script
 url = "https://books.toscrape.com/"
 response = requests.get(url)
+
+# Get status code to mark down issue
 print(response.status_code)
 
 soup = BeautifulSoup(response.text, "html.parser")
 books = soup.find_all("article", class_="product_pod")
 
+# Loops through each book for title and price
 for book in books:
     title = book.h3.a["title"]
     price = book.find("p", class_="price_color").text
-    Sorted_price = sorted(price)
-    print("\n"f"{title} - {Sorted_price}")
+
+    # Lists each books title and price
+    print("\n"f"{title} - {price}")
+
+with open("books.csv", "w", newline="") as f:
+    writer = csv.writer(f)
+    writer.writerow(["Title", "Price"])
+
+    for book in books:
+        title = book.h3.a["title"]
+        price = book.find("p", class_="price_color").text
+        writer.writerow([title, price])
